2160f677042ad6cedc3946cca17632173620ae52
GangneuxA <gangneux.alex@gmail.com>
2024-03-27 17:00:04 +0100

    tests user ok

--- users/tests/users.test.js
+++ users/tests/users.test.js
@@ -6,7 +6,7 @@ let userID = ''
 describe('User Controller', () => {
 
     it('should insert a new user', async () => {
-        const newUser = { email: 'test@example.com', firstname: 'John', lastname: 'Doe', password: 'password123' };
+        const newUser = { email: 'c', firstname: 'c', lastname: 'c', password: 'c' };
     
         const response = await fetch(`${URLUSER}`, {
             method: 'POST',
@@ -28,6 +28,8 @@ describe('User Controller', () => {
     
     it('should get all users', async () => {
         
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
         const response = await fetch(`${URLUSER}`, {
             method: 'GET',
             headers: {
@@ -41,6 +43,9 @@ describe('User Controller', () => {
     });
     
     it('should get a user by id', async () => {
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
         const id = userID +'/'
         const response = await fetch(`${URLUSER}${id}`, {
             method: 'GET',
@@ -54,19 +59,44 @@ describe('User Controller', () => {
     
     });
 
+    it('should update a user by ID', async () => {
 
-    // it('should update a user by ID', async () => {
-    // const updatedUser = { email: 'updated@example.com' };
-    // const response = await request(app)
-    //     .put('/users/'+userID)
-    //     .send(updatedUser);
-    // expect(response.status).toBe(200);
-    // // Ajoutez d'autres assertions ici pour vérifier la structure de la réponse et les données retournées
-    // });
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // it('should delete a user by ID', async () => {
-    // const response = await request(app).delete('/users/'+userID);
-    // expect(response.status).toBe(200);
-    // // Ajoutez d'autres assertions ici pour vérifier la structure de la réponse et les données retournées
-    // });
+        const id = userID +'/'
+        const updatedUser = { email: 'updated@example.com' };
+        const response = await fetch(`${URLUSER}${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        });
+
+        userEmail = await response.json();
+        console.log(userEmail)
+        userEmail = userEmail.email;
+    
+        expect(response.status).toBe(200);
+        expect(userEmail).toBe('updated@example.com');
+    
+    });
+
+    it('should delete a user by ID', async () => {
+
+        await new Promise(resolve => setTimeout(resolve, 1500));
+
+        const id = userID +'/'
+        const response = await fetch(`${URLUSER}${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
+    
+        expect(response.status).toBe(200);
+        expect(response.ok).toBe(true);
+    
+    });
 });
