6d81ade061df41fbb69247719d8043a7fff8516f
Axel Choteau <axelc37@outlook.fr>
2024-04-02 16:52:37 +0200

    Before tests, create user and fetch jwt

--- frontapi/tests/frontapi.test.js
+++ frontapi/tests/frontapi.test.js
@@ -2,26 +2,55 @@ require('dotenv').config()
 const URLEVENT = process.env.URLEVENT;
 const URLBUY = process.env.URLBUY;
 const URLUSER = process.env.URLUSER;
-const URLFRONT = process.env.URLFRONT;
+const URLLOGIN = process.env.URLLOGIN;
 let eventID = '';
-
-beforeAll(() => {
-   // Create a user and authenticate
-    console.log("Hello ??")
-});
-
-afterAll(() => {
-    // Delete the test user
-    console.log("Done.")
-});
+let userId = '';
+let jwt = '';
 
 describe('Event Controller', () => {
+    beforeAll(async () => {
+        // Create a user
+        const email = Math.random().toString(16).substring(2, 16) + '@example.test';
+        const newUser = {email: email, firstname: 'Test', lastname: 'Mann', password: 'password'};
+        const response = await fetch(`${URLUSER}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUser),
+        });
+        const userResponse = await response.json();
+        userId = userResponse.data._id;
+
+        // HACK : Manually wait for the user to be created
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // Authenticate as the new user
+        const credentials = {email: newUser.email, password: newUser.password}
+        const loginResponse = await fetch(`${URLLOGIN}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials),
+
+        })
+        const loginJson = await loginResponse.json();
+        jwt = loginJson.token;
+        console.debug(`Grabbed auth token : ${jwt}`);
+
+    });
+    afterAll(() => {
+        // Delete the test user
+        console.log("This is where we should delete user " + userId);
+    });
+
     it('should insert a new event', async () => {
-        const newEvent = { name: 'Event', desc: 'Description', numberDispo: 10, price: 20 };
+        const newEvent = {name: 'Event', desc: 'Description', numberDispo: 10, price: 20};
 
         const response = await fetch(URLEVENT, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(newEvent),
         });
 
@@ -37,7 +66,7 @@ describe('Event Controller', () => {
 
         const response = await fetch(`${URLEVENT}`, {
             method: 'GET',
-            headers: { 'Accept': 'application/json' },
+            headers: {'Accept': 'application/json'},
         });
 
         expect(response.status).toBe(200);
@@ -50,7 +79,7 @@ describe('Event Controller', () => {
 
         const response = await fetch(`${URLEVENT}/${eventID}`, {
             method: 'GET',
-            headers: { 'Accept': 'application/json' },
+            headers: {'Accept': 'application/json'},
         });
 
         expect(response.status).toBe(200);
@@ -61,11 +90,11 @@ describe('Event Controller', () => {
 
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        const updatedEvent = { name: 'update', desc: 'Description', numberDispo: 20, price: 20 };
+        const updatedEvent = {name: 'update', desc: 'Description', numberDispo: 20, price: 20};
 
         const response = await fetch(`${URLEVENT}/${eventID}`, {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
             body: JSON.stringify(updatedEvent),
         });
 
@@ -79,7 +108,7 @@ describe('Event Controller', () => {
 
         const response = await fetch(`${URLEVENT}/${eventID}`, {
             method: 'DELETE',
-            headers: { 'Accept': 'application/json' },
+            headers: {'Accept': 'application/json'},
         });
 
         expect(response.status).toBe(200);
