79d771e85208c46e7dea23d1b88da59763711b36
Axel Choteau <axelc37@outlook.fr>
2024-04-03 15:45:36 +0200

    FrontAPI Tests : FAILS - Buy tests (+ some light refactoring)

--- frontapi/tests/frontapi.test.js
+++ frontapi/tests/frontapi.test.js
@@ -1,59 +1,105 @@
 require('dotenv').config()
 const URLEVENT = process.env.URLEVENT;
+const URLEVENTSERVICE = process.env.URLEVENTSERVICE;
 const URLBUY = process.env.URLBUY;
 const URLUSER = process.env.URLUSER;
 const URLUSERSERVICE = process.env.URLUSERSERVICE;
 const URLLOGIN = process.env.URLLOGIN;
-let eventId = '';
+const DELETE_USER_AFTER_TESTS = process.env.DELETE_USER_AFTER_TESTS;
+let testEventId = '';
 let testUserId = '';
+let testBuyId = '';
 let jwt = '';
 let headers = {'Content-Type': 'application/json', 'Accept': 'application/json'};
 
 const randomString = Math.random().toString(16).substring(2, 16);
 
+async function createTestUserAndAuthenticate() {
+    // Create a user
+    const email = randomString + '@example.test';
+    const newUser = {email: email, firstname: 'Test', lastname: 'Mann', password: 'password', role: 'admin'};
+    const response = await fetch(`${URLUSERSERVICE}`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(newUser),
+    });
+    const userResponse = await response.json();
+    testUserId = userResponse._id;
+
+    // HACK : Manually wait for the user to be created
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Authenticate as the new user
+    const credentials = {email: newUser.email, password: newUser.password}
+    const loginResponse = await fetch(`${URLLOGIN}`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(credentials),
+
+    })
+    const loginJson = await loginResponse.json();
+    jwt = loginJson.token;
+    headers.Authorization = `Bearer ${jwt}`;
+    console.debug(`Grabbed auth token for test user : ${jwt}`);
+}
+
+async function deleteTestUser() {
+    // Delete the test user
+    const response = await fetch(`${URLUSER}`, {
+        method: 'DELETE',
+        headers: headers
+    });
+    const deleteResponse = await response.json();
+    console.debug("Deleting test user " + testUserId + " : " + deleteResponse.status);
+}
+
+async function createTestEvent() {
+    // Create an event
+    const newEvent = {
+        name: `Event-${randomString}`,
+        desc: 'Event created during automated testing.',
+        numberDispo: 10,
+        price: 20
+    };
+
+    const eventResponse = await fetch(URLEVENTSERVICE, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(newEvent),
+    });
+
+    const event = await eventResponse.json();
+    testEventId = event._id;
+}
+
+async function deleteTestEvent() {
+    // Delete the test user
+    const eventResponse = await fetch(`${URLEVENTSERVICE}/${testEventId}`, {
+        method: 'DELETE',
+        headers: headers
+    });
+    const eventDeleteResponse = await eventResponse.json();
+    console.debug(eventDeleteResponse);
+    console.debug("Deleting test event " + testEventId + " : " + eventDeleteResponse.status);
+}
+
 describe('Front API tests', () => {
-    // Before all tests : Get authentication token
+    // Before all tests : Create user and event
     beforeAll(async () => {
-        // Create a user
-        const email = randomString + '@example.test';
-        const newUser = {email: email, firstname: 'Test', lastname: 'Mann', password: 'password', role: 'admin'};
-        const response = await fetch(`${URLUSERSERVICE}`, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(newUser),
-        });
-        const userResponse = await response.json();
-        testUserId = userResponse._id;
-
-        // HACK : Manually wait for the user to be created
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        // Authenticate as the new user
-        const credentials = {email: newUser.email, password: newUser.password}
-        const loginResponse = await fetch(`${URLLOGIN}`, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(credentials),
-
-        })
-        const loginJson = await loginResponse.json();
-        jwt = loginJson.token;
-        headers.Authorization = `Bearer ${jwt}`;
-        console.debug(`Grabbed auth token for test user.`);
-
+        await createTestUserAndAuthenticate();
+        await createTestEvent();
     });
     // When tests are done : delete the test user
     afterAll(async () => {
-        // Delete the test user
-        const response = await fetch(`${URLUSER}`, {
-            method: 'DELETE',
-            headers: headers
-        });
-        const deleteResponse = await response.json();
-        console.log("Deleting test user " + testUserId + " : " + deleteResponse.status);
-
+        if (DELETE_USER_AFTER_TESTS) {
+            await deleteTestUser();
+        } else {
+            console.debug("Test user was NOT DELETED. Set DELETE_USER_AFTER_TESTS env var to true.");
+        }
+        await deleteTestEvent();
     });
 
+    // "event" tests
     it('should insert a new event', async () => {
         const newEvent = {name: `Event-${randomString}`, desc: 'Description', numberDispo: 10, price: 20};
 
@@ -64,7 +110,6 @@ describe('Front API tests', () => {
         });
 
         const event = await response.json();
-        eventId = event.data._id;
 
         expect(response.status).toBe(200);
     });
@@ -86,7 +131,7 @@ describe('Front API tests', () => {
 
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        const response = await fetch(`${URLEVENT}/${eventId}`, {
+        const response = await fetch(`${URLEVENT}/${testEventId}`, {
             method: 'GET',
             headers: headers,
         });
@@ -101,7 +146,7 @@ describe('Front API tests', () => {
 
         const updatedEvent = {name: 'update', desc: 'Description', numberDispo: 20, price: 20};
 
-        const response = await fetch(`${URLEVENT}/${eventId}`, {
+        const response = await fetch(`${URLEVENT}/${testEventId}`, {
             method: 'PUT',
             headers: headers,
             body: JSON.stringify(updatedEvent),
@@ -115,7 +160,7 @@ describe('Front API tests', () => {
 
         await new Promise(resolve => setTimeout(resolve, 1500));
 
-        const response = await fetch(`${URLEVENT}/${eventId}`, {
+        const response = await fetch(`${URLEVENT}/${testEventId}`, {
             method: 'DELETE',
             headers: headers,
         });
@@ -123,4 +168,50 @@ describe('Front API tests', () => {
         expect(response.status).toBe(200);
 
     });
+
+    // "buy" tests
+    it('should insert a new buy', async () => {
+        const newBuy = { user: testUserId, event: testEventId, count: 1 };
+
+        const response = await fetch(URLBUY, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(newBuy),
+        });
+
+        const createdBuy = await response.json();
+        // TODO : This fails with a cryptic error message :
+        //  "Failed to get account to"
+        console.debug(createdBuy);
+        testBuyId = createdBuy.data._id;
+
+        expect(response.status).toBe(201);
+    });
+
+
+    it('should get all buys', async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        const response = await fetch(URLBUY, {
+            method: 'GET',
+            headers: headers
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.ok).toBe(true);
+    });
+
+
+    it('should get buys by user ID', async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        const response = await fetch(`${URLBUY}/${testUserId}`, {
+            method: 'GET',
+            headers: headers
+        });
+
+        expect(response.status).toBe(200);
+        expect(response.ok).toBe(true);
+    });
+
 });
